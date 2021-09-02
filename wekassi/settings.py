decbe8c2aa1e17c504016da74b4676c7206b4d18
vikash8507 <71246736+vikash8507@users.noreply.github.com>
2021-09-02 19:29:06 +0530

    Update settings.py

--- wekassi/settings.py
+++ wekassi/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-f8amily6#xv)y-h+syjq09g2a0ru*9t^oygi9*-u0+4ikrxz-('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["https://wekassiauthapi.herokuapp.com/", "http://localhost:8000/"]
 
 
 # Application definition
