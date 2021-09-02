62be333185845e790cf8987de48ac374087a22cc
vikash8507 <71246736+vikash8507@users.noreply.github.com>
2021-09-02 19:39:07 +0530

    Update urls.py

--- wekassi/urls.py
+++ wekassi/urls.py
@@ -5,10 +5,10 @@ from django.urls import path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('api/register/', RegisterAPI.as_view(), name='register'),
-    path('api/login/', LoginAPI.as_view(), name='login'),
-    path('api/logout/', knox_views.LogoutView.as_view(), name='logout'),
-    # path('api/logoutall/', knox_views.LogoutAllView.as_view(), name='logoutall'),
-    path('api/change-password/', ChangePasswordView.as_view(),
+    path('/api/register/', RegisterAPI.as_view(), name='register'),
+    path('/api/login/', LoginAPI.as_view(), name='login'),
+    path('/api/logout/', knox_views.LogoutView.as_view(), name='logout'),
+    # path('/api/logoutall/', knox_views.LogoutAllView.as_view(), name='logoutall'),
+    path('/api/change-password/', ChangePasswordView.as_view(),
          name='change-password'),
 ]
