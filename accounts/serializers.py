d5bd2a0daea4c5973c68ab587e32cbf6d6579e55
vikash8507 <71246736+vikash8507@users.noreply.github.com>
2021-09-08 21:34:36 +0530

    Update serializers.py

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -14,12 +14,19 @@ class UserSerializer(serializers.ModelSerializer):
 
 
 class RegisterSerializer(serializers.ModelSerializer):
+    ROLES = (
+        ('driver', "Driver"),
+        ('company', "Company"),
+        ('user', 'User')
+    )
+    
     first_name = serializers.CharField(max_length=30, required=True)
     last_name = serializers.CharField(max_length=30, required=True)
+    role = serializers.ChoiceField(choices=ROLES, required=True)
 
     class Meta:
         model = User
-        fields = ('id', 'email', 'password', 'first_name', 'last_name')
+        fields = ('id', 'email', 'password', 'first_name', 'last_name', 'role')
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
@@ -27,6 +34,7 @@ class RegisterSerializer(serializers.ModelSerializer):
             validated_data['email'], validated_data['password'])
         user.first_name = validated_data['first_name']
         user.last_name = validated_data['last_name']
+        user.role = validated_data['role']
         user.save()
 
         return user
