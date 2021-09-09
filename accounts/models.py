6aaf2791fc19d535c93a02fc5b73b7982f133c4c
vikash8507 <71246736+vikash8507@users.noreply.github.com>
2021-09-09 20:31:43 +0530

    Add files via upload

---
+++ accounts/models.py
@@ -0,0 +1,59 @@
+from django.db import models
+from django.contrib.auth.models import BaseUserManager, AbstractUser
+from django.utils.translation import ugettext_lazy as _
+
+
+class UserManager(BaseUserManager):
+    """
+    Custom user model manager where email is the unique identifiers
+    for authentication instead of usernames.
+    """
+
+    def create_user(self, email, password, **extra_fields):
+        """
+        Create and save a User with the given email and password.
+        """
+        if not email:
+            raise ValueError(_('The Email must be set'))
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, password, **extra_fields):
+        """
+        Create and save a SuperUser with the given email and password.
+        """
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError(_('Superuser must have is_staff=True.'))
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError(_('Superuser must have is_superuser=True.'))
+        return self.create_user(email, password, **extra_fields)
+
+
+class User(AbstractUser):
+    ROLES = (
+        ('driver', "Driver"),
+        ('company', "Company"),
+        ('user', 'User')
+    )
+    
+    username = None
+    email = models.EmailField(_('email address'), unique=True)
+    role = models.CharField(_('user role'), max_length=25, choices=ROLES, default='user')
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
+
+    spouse_name = models.CharField(blank=True, max_length=100)
+    date_of_birth = models.DateField(blank=True, null=True)
+
+    def __str__(self):
+        return self.email
